98eff66a4faaca092dbe5459929e7567abd45be7
leush <leushkinml@mail.ru>
2022-11-24 00:11:00 +0300

    Initial commit

---
+++ src/main/java/com/skypro/employee/employeebooklionspring/EmployeeBookLionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.employee.employeebooklionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookLionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBookLionSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/employee/employeebooklionspring/EmployeeBookLionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.employee.employeebooklionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookLionSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
